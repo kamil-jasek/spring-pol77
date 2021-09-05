345a9f50134b5cd7e91dc6220e63e071e5f959c4
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-09-05 13:44:24 +0200

    test

--- src/main/java/pl/sda/customers/entity/CustomerRepository.java
+++ src/main/java/pl/sda/customers/entity/CustomerRepository.java
@@ -3,6 +3,7 @@ package pl.sda.customers.entity;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
@@ -11,4 +12,7 @@ public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     List<Person> findByFirstNameStartingWithIgnoreCaseAndLastNameStartingWithIgnoreCase(String firstName, String lastName);
 
     List<Customer> findByEmailIgnoreCaseEndingWith(String email);
+
+    @Query("from Person p where upper(p.firstName) like upper(?1) and upper(p.lastName) like upper(?2)")
+    List<Person> searchPeople(String firstName, String lastName);
 }
--- src/test/java/pl/sda/customers/entity/CustomerRepositoryTest.java
+++ src/test/java/pl/sda/customers/entity/CustomerRepositoryTest.java
@@ -72,4 +72,19 @@ class CustomerRepositoryTest {
         // then
         assertEquals(List.of(customer2, customer3), result);
     }
+
+    @Test
+    void shouldSearchPeople() {
+        // given
+        final var customer1 = new Person("ak@wp.pl", "Jan", "Nowak", "92929929929");
+        final var customer2 = new Person("qw@wp.pl", "Jan", "Kowalski", "83838288233");
+        final var customer3 = new Person("cx@wp.pl", "Janeczek", "Nowaczkiewicz", "83838288233");
+        repository.saveAllAndFlush(List.of(customer1, customer2, customer3));
+
+        // when
+        final var result = repository.searchPeople("jan%", "nowa%");
+
+        // then
+        assertEquals(List.of(customer1, customer3), result);
+    }
 }
