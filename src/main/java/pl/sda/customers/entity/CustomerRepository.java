8ae2d24f95c7f0cdde70dd8e783dbe60eb64e9b9
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-09-05 12:48:10 +0200

    test

--- src/main/java/pl/sda/customers/entity/CustomerRepository.java
+++ src/main/java/pl/sda/customers/entity/CustomerRepository.java
@@ -1,8 +1,10 @@
 package pl.sda.customers.entity;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
+    List<Person> findByLastName(String lastName); // --> select * from customers c where c.last_name = ?
 }
--- src/test/java/pl/sda/customers/entity/CustomerRepositoryTest.java
+++ src/test/java/pl/sda/customers/entity/CustomerRepositoryTest.java
@@ -27,4 +27,18 @@ class CustomerRepositoryTest {
         // then
         assertEquals(2, repository.count());
     }
+
+    @Test
+    void shouldFindPersonByLastname() {
+        // given
+        final var customer1 = new Person("ak@wp.pl", "Jan", "Nowak", "92929929929");
+        final var customer2 = new Person("qw@wp.pl", "Jan", "Kowalski", "83838288233");
+        repository.saveAllAndFlush(List.of(customer1, customer2));
+
+        // when
+        final var result = repository.findByLastName("Nowak");
+
+        // then
+        assertEquals(List.of(customer1), result);
+    }
 }
