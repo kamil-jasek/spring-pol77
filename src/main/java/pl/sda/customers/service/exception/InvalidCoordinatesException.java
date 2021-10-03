848d84fa4eda6d262cd8e7d54aaf960ef6dadd68
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-10-03 11:39:45 +0200

    test

---
+++ src/main/java/pl/sda/customers/service/exception/InvalidCoordinatesException.java
@@ -0,0 +1,8 @@
+package pl.sda.customers.service.exception;
+
+public final class InvalidCoordinatesException extends BusinessServiceException {
+
+    public InvalidCoordinatesException(String message) {
+        super(message);
+    }
+}
